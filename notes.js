56af41762ce18e1f818d16a6ff1ccbaef319a0ec
PommE15 <apple.chan-fardel@guardian.co.uk>
2015-07-06 16:36:46 +0100

    Test adding the note icon

--- notes.js
+++ notes.js
@@ -1,1 +1,10 @@
-alert('boo!');
+window.setTimeout(addNote, 2000);
+
+function addNote() {
+    
+    var noteEl = document.querySelector(".selection-sharing .social");
+    var noteHTML = noteEl.innerHTML;
+    noteEl.innerHTML = noteHTML + '<li class="social__item" data-link-name="note"><a class="rounded-icon social-icon social-icon--twitter js-selection-twitter" data-link-name="social-selection" target="_blank" href=""><span class="u-h">Share on Twitter</span><i class="i-share-twitter--white i"></i></a></li>';
+
+    //TODO: ..    
+};
