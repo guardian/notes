2bb7028aaf8e72f7f3200086f7fc0c64ae88082a
PommE15 <apple.chan-fardel@guardian.co.uk>
2015-07-07 08:44:34 +0100

    Add cancel button to the form

--- notes.js
+++ notes.js
@@ -1,19 +1,21 @@
-window.setTimeout(bootstrap, 2000);
+window.setTimeout(bootstrap, 3000);
+/*document.addEventListener("DOMContentLoaded", bootstrap});*/
 
 function bootstrap() {
 
     var noteEl = document.querySelector(".selection-sharing .social");
     var noteHTML = noteEl.innerHTML;
-    noteEl.innerHTML = noteHTML + '<li class="social__item" data-link-name="note"><a class="rounded-icon social-icon social-icon--note js-selection-twitter" data-link-name="social-selection" target="_blank" href="#"><span class="u-h">Share on Twitter</span><svg width="22" height="22" viewBox="0 0 20 20" style="padding: 10px 0 0 8px;"><path d="M13 0l1 1v7l-1 1H7l-2 3H4V9H2L1 8V1l1-1h11z" style="fill: white;"></path></svg></a></li>';
+    noteEl.innerHTML = noteHTML + '<li class="social__item" data-link-name="note"><a class="rounded-icon social-icon social-icon--note" target="_blank" href="#"><svg width="22" height="22" viewBox="0 0 20 20" style="padding: 10px 0 0 8px;"><path d="M13 0l1 1v7l-1 1H7l-2 3H4V9H2L1 8V1l1-1h11z" style="fill: white;"></path></svg></a></li>';
 
     $('.social-icon--note').click(displayNoteForm);
 }
 
 function displayNoteForm(event) {
-    var form = '<div class="d-comment-box__content">' +
+    var form = '<div class="d-comment-box__content js-note-input">' +
         '<div class="d-comment-box__messages"></div>' +
         '<textarea name="body" class="textarea d-comment-box__body" placeholder="Join the discussion…"></textarea>' +
         '<button type="submit" class="u-button-reset button button--large button--primary submit-input d-comment-box__submit" disabled="disabled">Post your comment</button>' +
+        '<button type="submit" class="u-button-reset button button--large button--primary submit-input d-comment-box__submit js-note-cancel">Cancel</button>' +
         '<span class="u-fauxlink d-comment-box__preview" role="button">Preview</span>' +
         '<span class="u-fauxlink d-comment-box__hide-preview" role="button">Hide preview</span>' +
         '<span class="u-fauxlink d-comment-box__cancel" role="button">Cancel</span>' +
@@ -37,6 +39,13 @@ function displayNoteForm(event) {
 
     event.preventDefault();
     p.innerHTML = p.innerHTML + form;
+    
+    $('.js-note-cancel').click(removeNoteForm);
+}
+
+function removeNoteForm() {
+    var formEl = document.querySelector(".js-note-input");
+    formEl.parentElement.removeChild(formEl);
 }
 
 function renderComments(node) {}
